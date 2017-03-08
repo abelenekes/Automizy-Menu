731a7318b8ce6daf3ea202475156592b61fa0c32
Enekes Abel <abel.enekes@protopmail.com>
2017-03-08 17:22:46 +0100

    closed menu style complete

--- src/js/elements/layout.js
+++ src/js/elements/layout.js
@@ -23,7 +23,7 @@ define([
 
         $AM.$tmp = $('<div id="automizy-menu-tmp"></div>');
 
-        $AM.$widget = $('<div id="automizy-menu"></div>');
+        $AM.$widget = $('<div id="automizy-menu" class="automizy-menu-closed"></div>');
         $AM.$widgetTable = $('<table cellpadding="0" cellspacing="0" border="0" id="automizy-menu-table"></table>').appendTo($AM.$widget);
 
         /*The menu header*/
@@ -42,7 +42,7 @@ define([
         $AM.$logoNormal = $('<img id="automizy-menu-logo-normal" src="" />').appendTo($AM.$logoBox);
         $AM.$mobileOpenCloseIcon = $('<span id="automizy-menu-mobile-openclose-icon"></span>').appendTo($AM.$widgetTop);
         $AM.$logoIcon = $('<img id="automizy-menu-logo-icon" src="" />').appendTo($AM.$logoBox);
-        $AM.$openCloseIcon = $('<span id="automizy-menu-openclose-icon" class="fa fa-outdent"></span>').appendTo($AM.$widgetTop);
+        $AM.$openCloseIcon = $('<span id="automizy-menu-openclose-icon" title="Close sidebar" class="fa fa-outdent"></span>').appendTo($AM.$widgetTop);
 
 
         $AM.$menuBox = $('<div id="automizy-menu-item-box"></div>').appendTo($AM.$menuItemList);
