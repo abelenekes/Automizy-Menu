156c77ee257705a70bb60995cbc201b07b2147e7
Réti György <gyorgy.reti@protopmail.com>
2017-03-08 11:31:37 +0100

    update

--- dist/automizy-menu.js
+++ dist/automizy-menu.js
@@ -581,7 +581,7 @@
     p.single = function(single){
         var t = this;
         if (typeof single !== 'undefined') {
-            t.d.single = $A.parseBoolean(single);
+            t.d.single = single || false;
             t.widget().toggleClass('automizy-menu-menuitem-single', t.d.single);
             t.setDisplay();
             return t;
@@ -601,7 +601,7 @@
     p.visibility = function(visibility){
         var t = this;
         if (typeof visibility !== 'undefined') {
-            t.d.visibility = $A.parseBoolean(visibility || false);
+            t.d.visibility = visibility || false;
             if (t.d.visibility) {
                 t.show();
             } else {
@@ -763,7 +763,7 @@
     p.visibility = function(visibility){
         var t = this;
         if (typeof visibility !== 'undefined') {
-            t.d.visibility = $A.parseBoolean(visibility || false);
+            t.d.visibility = visibility || false;
             if (t.d.visibility) {
                 t.show();
             } else {
